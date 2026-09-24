b082b92e45d86a4be72e1bc3b39d410bafb4ef0c
dusly007 <dusly30@gmail.com>
2026-09-23 21:40:35 -0400

    feat: ajout de l'animator et des animations Idle et Move du joueur

--- Assets/Scripts/MouvementRobot.cs
+++ Assets/Scripts/MouvementRobot.cs
@@ -1,37 +1,79 @@
 using UnityEngine;
-
+ 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class MouvementRobot : MonoBehaviour
 {
     [SerializeField] private float vitesse = 5f;
-
+ 
     private Rigidbody2D corps;
     private Vector2 direction;
-
+    private Animator animator;
+ 
     private void Awake()
     {
         corps = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
+ 
     private void Update()
     {
-        // TODO : reconstruire la lecture des commandes et la direction normalisée. 
+        // reconstruire la lecture des commandes et la direction normalisée.
+ 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+ 
+ 
         direction = new Vector2(horizontal, vertical).normalized;
 
-    }
+          if (horizontal > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (horizontal < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
 
+        if (direction.sqrMagnitude < 0.01f)
+        {
+            // À l'arrêt (Idle)
+            animator.SetInteger("DirectionAnim", 0);
+        }
+        else
+        {
+            // On regarde quelle direction est la plus forte (verticale ou horizontale)
+            if (Mathf.Abs(direction.y) > Mathf.Abs(direction.x))
+            {
+                if (direction.y > 0)
+                {
+                    animator.SetInteger("DirectionAnim", 2); // Vers le HAUT
+                }
+                else
+                {
+                    animator.SetInteger("DirectionAnim", 3); // Vers le BAS
+                }
+            }
+            else
+            {
+                animator.SetInteger("DirectionAnim", 1); // Vers la GAUCHE / DROITE
+            }
+        }
+       
+    }
+ 
     private void FixedUpdate()
     {
-        // TODO : déplacer le robot en tenant compte du temps physique.
+        // déplacer le robot en tenant compte du temps physique.
+ 
         corps.MovePosition(corps.position + direction * vitesse * Time.fixedDeltaTime);
+ 
     }
-
+ 
     /*
      * BANQUE DE LIGNES — GROUPE B
      * Les lignes ne sont pas dans le bon ordre.
      *
-     
+     *
+     *
+     *
+     *
      */
 }
+ 
